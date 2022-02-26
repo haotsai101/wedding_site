f799dfd8ee9a4ab3c01566446219a6f44b1140d2
haotsai101 <tsaizhihao@gmail.com>
2022-02-25 22:55:20 -0700

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='font-bold'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link font-bold"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Test
         </a>
-      </header>
+      </header> */}
+      <h1 className="text-8xl font-black underline">
+        Hello world!
+      </h1>
+
+      <div className='text-6xl'>test</div>
+
     </div>
   );
 }
